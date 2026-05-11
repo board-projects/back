08ac9fe9bd3e5d1b07fe58a2b2d94161351bb9e3
mahankabir <mahan.kabir@gmail.com>
2026-05-11 12:59:43 +0300

    feat(dev): add docker ro project.

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,11 +2,11 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'mahan',
-  password: 'mahan',
-  database: 'board',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT || '5432'),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   entities: [__dirname + '/../**/*.entity.js'],
   migrations: ['src/migrations/*.ts'],
   synchronize: false,
