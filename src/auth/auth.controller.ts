27b494e82fc668af2cf406ae7cfd1fbd94f70c2e
mahankabir <mahan.kabir@gmail.com>
2026-05-16 17:33:07 +0300

    fix(auth): store token to cookie.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -12,7 +13,23 @@ export class AuthController {
     }
 
     @Post('verify-otp') 
-    async verifyOtp(@Body('email') email: string, @Body('code') code: string) {
-        return this.authService.verifyOtp(email, code);
+    async verifyOtp(
+        @Body('email') email: string, 
+        @Body('code') code: string, 
+        @Res({ passthrough: true }) res: Response
+    ) {
+        const { token, user } = await this.authService.verifyOtp(email, code);
+        res.cookie('access_token', token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'lax',
+            domain: process.env.COOKIE_DOMAIN || 'localhost',
+            maxAge: 24 * 60 * 60 * 1000, // 1 day
+        });
+
+        return {
+            message: 'login successful',
+            user
+        };
     }
 }
