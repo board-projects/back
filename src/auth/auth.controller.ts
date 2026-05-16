3c2b47df211f00a668440b257bd91792e27633f9
mahankabir <mahan.kabir@gmail.com>
2026-05-16 21:44:48 +0300

    fix(auth): change auth cookie to client side.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Post, Body, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -16,17 +15,9 @@ export class AuthController {
     async verifyOtp(
         @Body('email') email: string, 
         @Body('code') code: string, 
-        @Res({ passthrough: true }) res: Response
     ) {
         const { token, user } = await this.authService.verifyOtp(email, code);
-        res.cookie('access_token', token, {
-            httpOnly: true,
-            secure: true,
-            sameSite: 'lax',
-            domain: process.env.COOKIE_DOMAIN || 'localhost',
-            maxAge: 24 * 60 * 60 * 1000, // 1 day
-        });
-
+    
         return {
             message: 'login successful',
             access_token: token,
